c34b61afc84822f01d393bbc1493682774a768db
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2021-11-25 17:14:26 +0100

    change the functions exported by default from package

--- src/expense_viewer/__init__.py
+++ src/expense_viewer/__init__.py
@@ -1,7 +1,7 @@
 """Starting file for the project"""
-from .main import get_expense_report, load_details_from_expense_stmt
+from .main import get_expense_report, load_details_from_all_expense_stmts
 
 __all__ = [
     "get_expense_report",
-    "load_details_from_expense_stmt",
+    "load_details_from_all_expense_stmts",
 ]
